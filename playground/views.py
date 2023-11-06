694535c98a12cc0b2bc9ddb4e13419f64b0a2e11
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-06 22:18:01 +0330

    Debuging in vscode and django_debug_toolbar

--- playground/views.py
+++ playground/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
+    x = 1
+    y = 2
     return render(request, 'hello.html', {'name': 'Mahdi'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = 'storefront.urls'
