9b1332ced313fd90e874196faa9664193aa55f7d
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-06 23:05:57 +0330

    Some models and fields

--- store/models.py
+++ store/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    #9999.99
+    price = models.DecimalField(max_digits=6 , decimal_places=2)
+    inventory_type = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, defualt=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    PENDING = 'P'
+    COMPLETE = 'C'
+    FAILED = 'F'
+
+    PAYMENT_CHOICES = [
+        (PENDING = 'P'),
+        (COMPLETE = 'C'),
+        (FAILED = 'F'),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default=P)
