f067127105f80915e337303d66522bf668c867bb
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-08 08:04:40 +0330

    tracking likes and generic relations

---
+++ likes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+class LikedItem(models.Model):
+    # What user likes what object.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Defined generic relationship.
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
