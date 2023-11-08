25c803680ebfca324d9986beb9765c5fd1673c13
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-08 07:55:41 +0330

    Tags models and generic relationship

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+class TaggedItem(models.Model):
+    # What tag applied to what object.
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # Defined generic relationship.
+    product = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
