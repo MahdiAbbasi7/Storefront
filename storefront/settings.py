e56a1a24cb1088f559c944c89bd06552ecdcf101
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-10 13:43:04 +0330

    Using MariaDB in django

--- storefront/settings.py
+++ storefront/settings.py
@@ -89,8 +89,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'mahdi',
+        'PASSWORD': '1',
     }
 }
 
