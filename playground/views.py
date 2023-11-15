be7a4582dbe16d4a6798e33f6137a46c274264cf
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-15 19:56:01 +0330

    selected and prefetch relations

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, OrderItem, Customer
 
 def say_hello(request):
     query_set = Product.objects.all()
@@ -16,4 +16,12 @@ def debug(request):
     product = Product.objects.all()[:5]
     # Limit : 5, 6, 7, 8, 9
     product = Product.objects.all()[5:10]
+
+    # query for get all products that are ordered and order by title.
+    query_set = Product.objects.filter(
+        id__in = OrderItem.objects.values_list('product_id').distinct()).order_by('title')
+    
+    # selected_related uses for one to one and foreignkeys.
+    # prefetch_related uses for many to many and reverse foreignkeys.
+    
     return render(request, 'sqll.html', {'products': list(query_set)})
