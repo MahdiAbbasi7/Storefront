023ece8809d804d60278aa47fc1fb9652a34a2fd
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-16 16:12:36 +0330

    custom mangeres

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, OrderItem, Order
-
+from tags.models import TaggedItem
 def say_hello(request):
-    query_set = Product.objects.all()
-    for product in query_set :
-        print(product)
+    TaggedItem.objects.get_tags_for(Product, 1)
     return render(request, 'hello.html', {'name': 'Mahdi'})
 
 def debug(request):
--- tags/models.py
+++ tags/models.py
@@ -3,10 +3,21 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        conten_type = ContentType.objects.get_for_model(obj_type)
+        return TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+                conten_type = conten_type,
+                object_id = obj_id
+            )
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager
     # What tag applied to what object.
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     # Defined generic relationship.
