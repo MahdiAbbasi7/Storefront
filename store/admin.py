ff502f3a88e8b3e50822c73db3b9baa7c89f6192
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-17 18:13:59 +0330

    add new column to product admin page

--- store/admin.py
+++ store/admin.py
@@ -4,10 +4,16 @@ from . import models
 # admin.site.register(models.Product, ProductAdmin)
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price']
+    list_display = ['title', 'price', 'inventory_status']
     list_editable = ['price']
     list_per_page = 15
 
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory_type < 10 : 
+            return 'LOW'
+        return 'OK'
+    
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
