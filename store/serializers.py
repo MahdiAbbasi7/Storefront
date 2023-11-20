c54c54e771277a34eee30c4aa05e4e05d8a14411
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-20 12:55:19 +0330

    Datamodel in serializers

--- store/serializers.py
+++ store/serializers.py
@@ -3,16 +3,16 @@ from rest_framework import serializers
 from .models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Collection
+        fields=['id', 'title']
 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'inventory_type','collections','price', 'price_with_tax']
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    inventory = serializers.IntegerField(source='inventory_type')
-    price = serializers.DecimalField(max_digits=6, decimal_places=2)
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_price_with_tax')
     collections = serializers.HyperlinkedRelatedField(
         view_name='collections-detail',
