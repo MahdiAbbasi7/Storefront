01b207e53c4477b6c3600c568ae2e2033ab1ecbf
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-21 22:40:28 +0330

    ModelViewSet

--- store/views.py
+++ store/views.py
@@ -4,44 +4,29 @@ from rest_framework.decorators import api_view, APIView
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-class ProductList(generics.ListCreateAPIView):
-    queryset = Product.objects.select_related('collections').all()
-    serializer_class = ProductSerializer
 
-    # def get_queryset(self):
-    #     return Product.objects.select_related('collections').all()
-    
-    # def get_serializer(self, *args, **kwargs):
-    #     # don't return ProductSerializer()
-    #     return ProductSerializer 
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
     
     def get_serializer_context(self):
         return {'request': self.request}
-
-
-class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
     
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if product.orderitems.count() > 0:
             return Response({'error':'Product cannot be deleted'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-        
+        return Response(status=status.HTTP_204_NO_CONTENT)        
 
 
-class CollectionList(generics.ListCreateAPIView):
-    queryset = Collection.objects.annotate(products_count=Count('products')).all().order_by('id')
-    serializer_class = CollectionSerializer
-    
 
-class CollectionDetail(generics.RetrieveUpdateDestroyAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
