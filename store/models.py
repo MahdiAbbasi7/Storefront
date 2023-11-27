3fc37a6be32155695918e8b0615431b4826f25ad
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-27 18:48:10 +0330

    Creatin user proile

--- store/models.py
+++ store/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib import admin
 from django.db import models
 from django.core.validators import MinValueValidator
 from uuid import uuid4
@@ -51,18 +53,23 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
     
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+    
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['user__first_name', 'user__last_name']
 
 
 class Order(models.Model):
@@ -78,7 +85,7 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default=PENDING)
-    customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+    customer = models.ForeignKey(Customer,on_delete=models.PROTECT, related_name='orders')
 
 class OrderItem(models.Model):
     # for reverse relationship this name is conventions orderitem_set.(related_name)
@@ -113,4 +120,3 @@ class Review(models.Model):
     name = models.CharField(max_length=255)
     descriptions = models.TextField()
     data = models.DateField(auto_now_add=True)
-
