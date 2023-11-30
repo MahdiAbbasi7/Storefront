43d4897d534a43b684320efec091a93c62da345f
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-11-30 19:43:48 +0330

    Getting the currentuser (mod header)

--- storefront/settings.py
+++ storefront/settings.py
@@ -62,7 +62,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
-}
+   'ACCESS_TOKEN_LIFETIME':timedelta(days=1)
+}   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -162,9 +163,6 @@ AUTH_USER_MODEL = 'core.User'
 DJOSER = {
     'SERIALIZERS':{
         'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer',        
     }
-}
-
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME':timedelta(days=1)
 }
