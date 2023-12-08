fa9ef8407d390f0ede3064768919918ec6500cd5
Ali Haddadi <ali.haddadi154@gmail.com>
2023-12-08 22:56:20 +0330

    adding [OrderItemSerializer , OrderSerializer, CreateOrderSerializer] serializers

--- store/serializers.py
+++ store/serializers.py
@@ -107,3 +107,28 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer 
         fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
+
+   # for readability added item serializer
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+
+    class Meta:
+        model = OrderItem
+        fields = ['id','product','quantity','unit_price'] 
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'placed_at', 'payment_status', 'customer', 'items']
+
+class CreateOrderSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+
+    def save(self, **kwargs):
+        print(self.validated_data['cart_id'])
+        print(self.context['user_id'])
+
+        (customer, create) = Customer.objects.get_or_create(user_id=self.context['user_id'])
+        Order.objects.create(customer=customer)
--- store/views.py
+++ store/views.py
@@ -110,3 +110,5 @@ class CustomerViewSet(ModelViewSet):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data)
+
+
