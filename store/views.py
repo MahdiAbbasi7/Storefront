a06345c9465b9124803bfb0966f270602ffc846f
Ali Haddadi <ali.haddadi154@gmail.com>
2023-12-08 22:56:46 +0330

    added orderViewSet

--- store/views.py
+++ store/views.py
@@ -111,4 +111,25 @@ class CustomerViewSet(ModelViewSet):
             serializer.save()
             return Response(serializer.data)
 
+class OrderViewSet(ModelViewSet):
+    serializer_class = OrderSerializer
+    # permission_classes =[IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        return OrderSerializer
+    
+    
+    def get_serializer_context(self):
+        return {'user_id': self.request.user.id}
+    
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_staff:
+            return Order.objects.all()
+        (customer_id, created) = Customer.objects.only('id').get_or_create(user_id=user.id)
+        return Order.objects.filter(customer_id=customer_id)
 
