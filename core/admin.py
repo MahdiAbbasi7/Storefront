59efb07f316c42341e0ced2160fbf5c91828a078
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-12-11 19:13:50 +0330

    Add images fields to Admin Panel

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from django.contrib.contenttypes.admin import GenericTabularInline
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from store.models import Product
 from tags.models import TaggedItem
-from store.admin import ProductAdmin
+from store.admin import ProductAdmin, ProductImageInline
 from .models import User
 
 @admin.register(User)
@@ -23,7 +23,7 @@ class TagInline(GenericTabularInline):
     autocomplete_fields = ['tag']
 
 class CustomProductAdmin(ProductAdmin):
-    inlines = [TagInline]
+    inlines = [TagInline, ProductImageInline]
 
 admin.site.unregister(Product)
 admin.site.register(Product,CustomProductAdmin)
--- store/admin.py
+++ store/admin.py
@@ -21,6 +21,15 @@ class InventoryFilter(admin.SimpleListFilter):
             return queryset.filter(inventory__lt=10)
 
 
+class ProductImageInline(admin.TabularInline):
+    model = models.ProductImage
+    readonly_fields=['tumbnail']
+
+    def tumbnail(self, instance):
+        if instance.image.name!='':
+            return format_html(f'<img src="{instance.image.url}" class="tumbnail" />')
+        return ''
+
 
 # admin.site.register(models.Product, ProductAdmin)
 @admin.register(models.Product)
@@ -31,6 +40,7 @@ class ProductAdmin(admin.ModelAdmin):
         'slug':['title'],
     }
     actions=['clear_inventory']
+    inlines=[ProductImageInline]
     list_display = ['title', 'price', 
                     'inventory_status', 'collections',]
     list_editable = ['price']
@@ -56,6 +66,11 @@ class ProductAdmin(admin.ModelAdmin):
             f'{updated_count} products were successfully updated.',
             messages.ERROR
         )
+
+    class Media:
+        css = {
+            'all':['store/styles.css']
+        }
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
