5670eb951ba8a5b1eebe0c7b7bdd4b860de97f2a
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-12-12 21:13:45 +0330

    Sending Emails with smtp4dev

--- playground/views.py
+++ playground/views.py
@@ -1,27 +1,30 @@
+from django.http import BadHeaderError
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem, Order
-from tags.models import TaggedItem
+from django.core.mail import mail_admins, send_mail
+
 def say_hello(request):
-    TaggedItem.objects.get_tags_for(Product, 1)
+    try:
+        send_mail('subject', 'message', 'storefront@uni.com', ['test@mahdi.com'])
+    except BadHeaderError:
+        raise BadHeaderError('This is bad method!')
     return render(request, 'hello.html', {'name': 'Mahdi'})
 
-def debug(request):
-    # None : if we use first()
-    # Boolean : if we use exists()
-    query_set = Product.objects.filter(price__range=(200, 300))
-    # Limit : 0, 1, 2, 3, 4
-    product = Product.objects.all()[:5]
-    # Limit : 5, 6, 7, 8, 9
-    product = Product.objects.all()[5:10]
+# def debug(request):
+#     # None : if we use first()
+#     # Boolean : if we use exists()
+#     query_set = Product.objects.filter(price__range=(200, 300))
+#     # Limit : 0, 1, 2, 3, 4
+#     product = Product.objects.all()[:5]
+#     # Limit : 5, 6, 7, 8, 9
+#     product = Product.objects.all()[5:10]
 
-    # query for get all products that are ordered and order by title.
-    query_set = Product.objects.filter(
-        id__in = OrderItem.objects.values_list('product_id').distinct()).order_by('title')
+#     # query for get all products that are ordered and order by title.
+#     query_set = Product.objects.filter(
+#         id__in = OrderItem.objects.values_list('product_id').distinct()).order_by('title')
     
-    # selected_related uses for one to one and foreignkeys.
-    # prefetch_related uses for many to many and reverse foreignkeys.
-    # Get the last 5 orders with their customer and itmes (inc product)
-    query_set = Order.objects.select_related('customer').order_by('-placed_at')[:5]
+#     # selected_related uses for one to one and foreignkeys.
+#     # prefetch_related uses for many to many and reverse foreignkeys.
+#     # Get the last 5 orders with their customer and itmes (inc product)
+#     query_set = Order.objects.select_related('customer').order_by('-placed_at')[:5]
 
-    return render(request, 'sqll.html', {'products': list(query_set)})
+#     return render(request, 'sqll.html', {'products': list(query_set)})
--- storefront/settings.py
+++ storefront/settings.py
@@ -180,3 +180,10 @@ DJOSER = {
         'current_user': 'core.serializers.UserSerializer',        
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'storefront@uni.com'
