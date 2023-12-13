41d6692e82118418fcf0aa5509f91f6756d633b0
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-12-13 19:08:28 +0330

    Define Schdule Tasks, Monitoring celery

--- storefront/settings.py
+++ storefront/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -194,3 +195,11 @@ ADMINS = [
 
 
 CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BEAT_SCHEDULE = {
+    'notify_customers':{
+        'task':'playground.tasks.notify_customers',
+        'schedule': 5, #crontab(day_of_week=1, hour=7, minute=30) # every monday at 7:30
+        'args': ['Hello World'],
+        # kwargs: {}
+    }
+}
