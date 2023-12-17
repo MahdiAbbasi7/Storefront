43ba9a3b445cffa6be04238985129a6e37921a30
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-12-17 13:39:24 +0330

    Some Test for collection endpoint

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
@@ -10,3 +11,26 @@ class TestCreateCollection:
         client = APIClient()
         response = client.post('/store/collections/', {'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_user_if_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_data_is_not_valid_returns_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': ''})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_data_is_valid_returns_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': 'a'})
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
+
+    
+        
