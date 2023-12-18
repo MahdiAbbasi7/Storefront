ceafea1cba792c585b0fe2093cea424db9e31dff
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-12-18 23:52:21 +0330

    Creating a Test Script for test perfomance of project (locust)

---
+++ locustfiles/browse_products.py
@@ -0,0 +1,33 @@
+from locust import HttpUser, task, between
+from random import randint
+
+class WebSiteUs(HttpUser):
+    wait_time = between(1, 5)
+    # Viewing products
+    @task(2)
+    def view_products(self):
+        collection_id = randint(2, 6)
+        self.client.get(
+            f'/store/products/?collection_id={collection_id}',
+            name='/store/products/')
+    # Viewing product details
+    @task(4)
+    def view_product_details(self):
+        prodcut_id = randint(1, 1000)
+        self.client.git(
+            f'/store/products/{prodcut_id}',
+            name='/store/products/'
+        )
+    # Add product to cart
+    @task(1)
+    def add_product(self, product):
+        product_id = randint(1, 10)
+        self.client.post(
+            f'/store/carts/{self.cart_id}/items',
+            name='/store/carts/items',
+            json={'product_id': product_id, 'quantity': 1}
+        )
+    def on_start(self):
+        response = self.client.post('/store/carts')
+        result = response.json()
+        self.cart_id = result['id'] 
