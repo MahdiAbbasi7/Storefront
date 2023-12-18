c58c7ae40de6c0e75d157495d431c56ffaa1d818
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-12-19 00:16:32 +0330

    Running perfomance testing

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -6,28 +6,31 @@ class WebSiteUs(HttpUser):
     # Viewing products
     @task(2)
     def view_products(self):
+        print('Veiw Products')
         collection_id = randint(2, 6)
         self.client.get(
-            f'/store/products/?collection_id={collection_id}',
-            name='/store/products/')
+            f'/store/products/?collections_id={collection_id}',
+            name='/store/products')
     # Viewing product details
     @task(4)
     def view_product_details(self):
+        print('Veiw Product Details')
         prodcut_id = randint(1, 1000)
-        self.client.git(
+        self.client.get(
             f'/store/products/{prodcut_id}',
-            name='/store/products/'
+            name='/store/products/:id'
         )
     # Add product to cart
     @task(1)
-    def add_product(self, product):
+    def add_to_cart(self):
+        print('Add to cart')
         product_id = randint(1, 10)
         self.client.post(
-            f'/store/carts/{self.cart_id}/items',
+            f'/store/carts/{self.cart_id}/items/',
             name='/store/carts/items',
             json={'product_id': product_id, 'quantity': 1}
         )
     def on_start(self):
-        response = self.client.post('/store/carts')
+        response = self.client.post('/store/carts/')
         result = response.json()
         self.cart_id = result['id'] 
