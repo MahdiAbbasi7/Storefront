e801ee2326d8338474586b1dc1c786030ff4427e
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-12-21 14:45:53 +0330

    Config logging

--- storefront/settings.py
+++ storefront/settings.py
@@ -221,3 +221,32 @@ CACHES = {
         }
     }
 }
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'general.log',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'file'],
+            'level': os.environ.get('DJANGO_LOG_LEVEL', 'INFO')
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '{asctime} ({levelname}) - {name} - {message}',
+            'style': '{' 
+        }
+    }
+}
