158816be0c71193dc5be148986ef028e7aaa036c
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-12-21 14:11:10 +0330

    Static files for home page

--- storefront/settings.py
+++ storefront/settings.py
@@ -168,6 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
