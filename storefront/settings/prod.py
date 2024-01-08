6038bd3de1319fb482ac82c5a5e4e60f1a27ee67
Ali Haddadi <72549224+Ali-H1@users.noreply.github.com>
2024-01-08 16:18:15 +0330

    Update prod.py

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.alihaddadi.sadrazkh.ir","193.164.4.226"]
 
 # DATABASES = {
 #     'default': dj_database_url.config()
