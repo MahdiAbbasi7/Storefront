7b3c89cade45a371ee3fdb9ac2c307065c2b3627
Ali Haddadi <72549224+Ali-H1@users.noreply.github.com>
2024-01-13 18:49:14 +0330

    Update prestart.sh

--- prestart.sh
+++ prestart.sh
@@ -1,5 +1,7 @@
 # Run collect static and migrations
+echo "creating suuuuuuuuper user"
+python manage.py createsuperuser --username admin --email admin@example.com --password admin
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py createsuperuser --username admin --email admin@example.com --password admin
+
 
