19a636e7571c2d32816075c2c055e070be40429a
Ali Haddadi <72549224+Ali-H1@users.noreply.github.com>
2024-01-13 18:55:57 +0330

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ echo "Apply database migrations"
 python manage.py migrate
 
 echo "creating suuuuuuuuper user"
-python manage.py createsuperuser --username admin --email admin@example.com --password admin
+python manage.py createsuperuser --username admin --email admin@example.com --noinput
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
