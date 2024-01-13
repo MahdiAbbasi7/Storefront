22daa08107455d9d358c40a3bfdadc2cb0fc90a2
Ali Haddadi <72549224+Ali-H1@users.noreply.github.com>
2024-01-13 18:34:19 +0330

    Update prestart.sh

--- prestart.sh
+++ prestart.sh
@@ -1,4 +1,4 @@
 # Run collect static and migrations
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py seed_db
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
